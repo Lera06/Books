e9a9a807ed6a78aabf3807239ba65573e8a11fc2
Lera06 <lera06@rambler.ru>
2023-08-07 17:54:36 +0300

    Commit Image Uploads

--- config/settings.py
+++ config/settings.py
@@ -199,4 +199,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 
+# Images
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
 
